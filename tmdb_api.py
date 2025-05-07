64190d1f0a33d26669021e24d38ef4ac6110c7bf
somya sharma <114328620+Somyasharmatech@users.noreply.github.com>
2025-05-07 23:40:55 +0530

    Update tmdb_api.py

--- tmdb_api.py
+++ tmdb_api.py
@@ -3,7 +3,7 @@ import os
 import streamlit as st
 
 # TMDB API configuration
-TMDB_API_KEY = st.secrets["TMDB_API_KEY"]["key"
+TMDB_API_KEY = st.secrets["TMDB_API_KEY"]["key"]
 TMDB_BASE_URL = "https://api.themoviedb.org/3"
 TMDB_IMAGE_BASE_URL = "https://api.themoviedb.org/3"
 
